672446b0b9847b03852c940813faee3749709220
boris <boris.lok.tw@outlook.com>
2021-12-30 00:04:50 +0800

    [Feature] Add database schema.

--- migrations/20211224070505_1.down.sql
+++ migrations/20211224070505_1.down.sql
@@ -1,1 +1,5 @@
 -- Add down migration script here
+
+drop index if exists idx_users_name;
+drop table if exists users;
+drop table if exists tokens;
--- migrations/20211224070505_1.up.sql
+++ migrations/20211224070505_1.up.sql
@@ -1,1 +1,20 @@
 -- Add up migration script here
+
+create table users
+(
+    id         uuid         not null primary key,
+    name       varchar(64)  not null,
+    password   varchar(128) not null,
+    role       smallint     not null,
+    created_at timestamptz,
+    updated_at timestamptz
+);
+
+create index idx_users_name on users using btree (name);
+
+create table tokens
+(
+    user_id    uuid         not null primary key,
+    token      varchar(512) not null,
+    expired_at timestamptz  not null
+);
