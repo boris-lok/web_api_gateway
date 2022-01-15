1b0af4ff77114d0065a0b786ca4ac97cb6a768a5
boris <boris.lok.tw@outlook.com>
2022-01-15 23:16:37 +0800

    [Feature] set cookie when user login/logout/renew token.

--- src/auth/route.rs
+++ src/auth/route.rs
@@ -2,7 +2,7 @@ use warp::{Filter, Reply};
 use warp::filters::BoxedFilter;
 
 use crate::auth::handlers::v1::{login_handler, logout_handler, renew_handler};
-use crate::core::middlewares::authorization::authenticated;
+use crate::core::middlewares::authorization::{authenticated_from_cookie};
 use crate::core::middlewares::with_env::with_env;
 use crate::Environment;
 
@@ -15,13 +15,13 @@ pub fn routes(env: Environment) -> BoxedFilter<(impl Reply,)> {
 
     let logout_route = warp::path!("api" / "v1" / "logout")
         .and(warp::post())
-        .and(authenticated(env.clone()))
+        .and(authenticated_from_cookie(env.clone()))
         .and(with_env(env.clone()))
         .and_then(logout_handler);
 
     let renew_route = warp::path!("api" / "v1" / "token" / "renew")
         .and(warp::post())
-        .and(authenticated(env.clone()))
+        .and(authenticated_from_cookie(env.clone()))
         .and(with_env(env))
         .and_then(renew_handler);
 
